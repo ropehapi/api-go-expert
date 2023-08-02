53eac83dd0c5a13f271c1aa41bb8adaaec8d7773
ropehapi <ropehapi@gmail.com>
2023-08-01 21:23:11 -0300

    Creating a Middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/ropehapi/api-go-expert/configs"
 	"github.com/ropehapi/api-go-expert/internal/entity"
 	"github.com/ropehapi/api-go-expert/internal/infra/database"
@@ -30,14 +32,29 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/product/{id}", productHandler.GetProduct)
-	r.Post("/product", productHandler.CreateProduct)
-	r.Put("/product/{id}", productHandler.UpdateProduct)
-	r.Delete("/product/{id}", productHandler.DeleteProduct)
+	r.Use(middleware.Recoverer)
+	r.Use(LogRequest)
+
+	r.Route("/product", func(r chi.Router)  {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)	
+	})
 
 	r.Post("/users/generate_token", userHandler.GetJWT)
 	r.Post("/user", userHandler.Create)
 
 	http.ListenAndServe("127.0.0.1:8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
